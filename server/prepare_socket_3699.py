e747d96ee55b48be8dd55eb3134887b32ee8972c
Swayam595 <44289328+Swayam595@users.noreply.github.com>
2022-12-13 08:57:55 +0530

    Add files via upload

--- server/prepare_socket_3699.py
+++ server/prepare_socket_3699.py
@@ -20,16 +20,18 @@ def prepare_socket_3699():
         t.start()
 
     try:
+        # Create a TCP/IPv4 socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Connect the socket to the port where the server is listening
         server_address = ('', 3699)
         print ('connecting to %s port %s' % server_address)
         sock.bind(server_address)
-
+        
+        # Listen for incoming connections
+        sock.listen(5)
+        
         while True:
-            # Listen for incoming connections
-            sock.listen(1)
             # Wait for a connection
             print ('waiting for a connection')
             connection, client_address = sock.accept()
