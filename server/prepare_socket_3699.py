bc5501f14815c0b536ebc172b95b0d538f50de3e
Swayam595 <44289328+Swayam595@users.noreply.github.com>
2022-12-13 09:50:44 +0530

    Update prepare_socket_3699.py

--- server/prepare_socket_3699.py
+++ server/prepare_socket_3699.py
@@ -28,10 +28,10 @@ def prepare_socket_3699():
         print ('connecting to %s port %s' % server_address)
         sock.bind(server_address)
         
-        # Listen for incoming connections
-        sock.listen(5)
-        
         while True:
+            # Listen for incoming connections
+            sock.listen(1)
+            
             # Wait for a connection
             print ('waiting for a connection')
             connection, client_address = sock.accept()
